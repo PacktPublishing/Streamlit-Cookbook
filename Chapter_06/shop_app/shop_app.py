a01d7b0db7e6dddef80179634eb27ad24f54f57c
edsaa <esaavedrac@u.northwestern.edu>
2024-07-01 17:03:16 -0500

    Code snippets for chapter 6

--- Chapter_06/shop_app/shop_app.py
+++ Chapter_06/shop_app/shop_app.py
@@ -1,14 +1,6 @@
 import streamlit as st
 from itertools import cycle
-from store_utils import Product, ShoppingCart, run_payment
-import json
-
-if "cart" not in st.session_state:
-    st.session_state.cart = ShoppingCart()
-
-
-if "product_db" not in st.session_state:
-    st.session_state.product_db = {}
+from store_utils import create_items_db, ShoppingCart
 
 
 def main():
@@ -16,14 +8,26 @@ def main():
         page_title="The Little Store Online", page_icon="🛒", layout="wide"
     )
 
+    if "cart" not in st.session_state:
+        st.session_state.cart = ShoppingCart()
+
+    if "items_db" not in st.session_state:
+        st.session_state.items_db = {}
+        create_items_db()
+
     ## Page layout
     st.header(":rainbow[The Little Store] ***:gray[Online]***", divider="grey")
     slogan_column, cart_column = st.columns([8, 2], vertical_alignment="bottom")
     items_area = st.container()
+    st.divider()
+    store_information_area = st.container()
 
     ## Page title and introduction
     with slogan_column:
         st.write("Welcome to the little store.")
+
+    with store_information_area:
+        st.caption("🔪 This is a recipe from the Streamlit Cookbook")
 
     ## Shopping cart interface
     with cart_column:
@@ -31,76 +35,27 @@ def main():
             "🛒",
             help=":gray[*Shopping cart*]",
             use_container_width=True,
-        ):  
+        ):
             st.write("🛒 _Shopping Cart_")
             st.session_state.cart.render_short_summary()
+
             if st.button(
                 "Go to checkout",
                 use_container_width=True,
-                help=f"Your total is *:green[${st.session_state.cart.total}]*",
-                disabled=not st.session_state.cart
+                help=f"Your total is *:green[${st.session_state.cart.total:.2f}]*",
+                disabled=not st.session_state.cart,
             ):
-                show_shopping_cart_in_dialog()
-
-    ## Read products and assemble a database
-    with open("products.json", "r") as f:
-        products = json.load(f)
-        for product in products:
-            Product(**product)
+                st.session_state.cart.show()
 
     ## Item description area
     with items_area:
-
         ## Create columns in the main section
-        products_cols = cycle(st.columns(2))
-        
-        for col, product in zip(products_cols, st.session_state.product_db.values()):
+        items_cols = cycle(st.columns(2))
+
+        for col, item in zip(items_cols, st.session_state.items_db.values()):
             with col:
-                product.render()
+                item.render()
 
 
-@st.experimental_dialog("🛒 Shopping cart", width="large")
-def show_shopping_cart_in_dialog():
-    """Dialog to show the shopping cart and options to pay"""
-
-    st.dataframe(
-        st.session_state.cart.summary,
-        use_container_width=True,
-        hide_index=True,
-        column_config={
-            "sku": None,
-            "name": "Name",
-            "price": st.column_config.NumberColumn(label="Price $", format=r"$%.2f"),
-            "image": st.column_config.ImageColumn(label="Image", width=20),
-            "qty": st.column_config.NumberColumn(label="Quantity", format=r"%.0f"),
-        },
-    )
-
-    empty_cart_column, pay_column = st.columns(2)
-
-    with empty_cart_column:
-        if st.button(
-            "Empty cart",
-            disabled=not st.session_state.cart,
-            use_container_width=True,
-        ):
-            del st.session_state.cart
-            st.rerun()
-
-    with pay_column:
-        total = st.session_state.cart.total
-
-        pay_button = st.button(
-            f"Pay total: ${total:.2f}",
-            disabled=not st.session_state.cart,
-            type="primary",
-            use_container_width=True,
-        )
-
-    if pay_button:
-        run_payment()
-        st.rerun()
-
-
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
--- Chapter_06/shop_app/store_utils.py
+++ Chapter_06/shop_app/store_utils.py
@@ -1,24 +1,21 @@
-from dataclasses import dataclass
-from collections import OrderedDict
 import streamlit as st
+from dataclasses import dataclass
+from collections import UserDict
 from time import sleep
+import json
 
 
 @dataclass
-class Product:
+class Item:
     sku: str
     name: str
-    brand: str
     price: float
     description: str
     image: str
 
     def __post_init__(self):
         # Adds the product to the database dictionary
-        st.session_state.product_db[self.sku] = self
-
-    def __hash__(self) -> int:
-        return hash(self.sku)
+        st.session_state.items_db[self.sku] = self
 
     def render(self):
         with st.container(border=True):
@@ -34,19 +31,17 @@ class Product:
 
             if st.session_state.cart.get(self.sku, False):
                 button_cols = st.columns([1, 5])
-                
+
                 with button_cols[1]:
                     self.render_add_to_cart_button()
-                
+
                 with button_cols[0]:
                     self.render_remove_from_cart_button()
 
             else:
                 self.render_add_to_cart_button()
 
-
     def render_add_to_cart_button(self):
-        
         st.button(
             "**➕ Add to cart**",
             key=f"btn_add_{self.sku}",
@@ -54,16 +49,6 @@ class Product:
             type="primary",
             on_click=self.add_to_cart_callback,
             help="*Add to cart*",
-        )
-
-    def render_remove_from_cart_button(self):
-        st.button(
-            "**−**",
-            key=f"btn_del_{self.sku}",
-            use_container_width=True,
-            on_click=self.remove_from_cart_callback,
-            disabled=not st.session_state.cart.get(self.sku, False),
-            help="*Remove from cart*",
         )
 
     def add_to_cart_callback(self):
@@ -75,6 +60,16 @@ class Product:
 
         st.toast(
             f"{self.name} was added to the cart.", icon=":material/add_shopping_cart:"
+        )
+
+    def render_remove_from_cart_button(self):
+        st.button(
+            "**−**",
+            key=f"btn_del_{self.sku}",
+            use_container_width=True,
+            on_click=self.remove_from_cart_callback,
+            disabled=not st.session_state.cart.get(self.sku, False),
+            help="*Remove from cart*",
         )
 
     def remove_from_cart_callback(self):
@@ -90,51 +85,94 @@ class Product:
             )
 
 
-class ShoppingCart(OrderedDict):
-
-    def render_short_summary(self):
-        names = self.summary["name"]
-        prices = self.summary["price"]
-        qtys = self.summary["qty"]
-        
-        for name, price, qty in zip(names, prices, qtys):
-            # st.markdown(f"**{name}**: \${price:.2f} × {qty:.0f} = \${price * qty:.2f}")
-            st.markdown(f":gray[- {name} (× {qty:.0f})]")
-
-
-
+class ShoppingCart(UserDict):
     @property
     def summary(self):
-        db = st.session_state.product_db
-        skus = list(self.keys())
-        qtys = list(self.values())
+        db = st.session_state.items_db
+        skus = self.keys()
 
         return {
             "sku": skus,
             "name": [db[sku].name for sku in skus],
             "image": [db[sku].image for sku in skus],
             "price": [db[sku].price for sku in skus],
-            "qty": qtys,
+            "qty": list(self.values()),
         }
 
     @property
     def total(self):
+        db = st.session_state.items_db
         qtys = self.values()
-        prices = [st.session_state.product_db[sku].price for sku in self.keys()]
+        prices = (db[sku].price for sku in self.keys())
 
         return sum([q * p for q, p in zip(qtys, prices)])
 
+    def render_short_summary(self):
+        names = self.summary["name"]
+        qtys = self.summary["qty"]
 
-def run_payment():
-    """Emulates some payment process that takes a few seconds"""
+        for name, qty in zip(names, qtys):
+            st.markdown(f":gray[- {name} (× {qty:.0f})]")
 
-    with st.status("💵 Initiating payment...", expanded=False) as status:
-        sleep(1)
-        status.update(label="💰 Grabbing your wallet...")
-        sleep(2)
-        status.update(label="💸 Paying...")
-        sleep(1)
-        status.update(label="✅ Payment complete!", state="complete", expanded=False)
-        sleep(1)
+    @st.experimental_dialog("🛒 Shopping cart", width="large")
+    def show(self):
+        """Dialog to show the shopping cart and options to pay"""
 
-        del st.session_state.cart
+        st.dataframe(
+            self.summary,
+            use_container_width=True,
+            hide_index=True,
+            column_config={
+                "sku": None,
+                "name": "Name",
+                "price": st.column_config.NumberColumn(
+                    label="Price $", format=r"$%.2f"
+                ),
+                "image": st.column_config.ImageColumn(label="Image"),
+                "qty": st.column_config.NumberColumn(label="Quantity", format=r"%.0f"),
+            },
+        )
+
+        empty_cart_column, pay_column = st.columns([1,3])
+
+        with empty_cart_column:
+            if st.button(
+                "Empty cart",
+                disabled=not self,
+                use_container_width=True,
+            ):
+                del st.session_state.cart
+                st.rerun()
+
+        with pay_column:
+
+            pay_button = st.button(
+                f"Pay total: ${self.total:.2f}",
+                disabled=not self,
+                type="primary",
+                use_container_width=True,
+            )
+
+        if pay_button:
+            self.run_payment()
+            del st.session_state.cart
+            st.rerun()
+
+    def run_payment(self):
+        with st.status("💵 Initiating payment...", expanded=False) as status:
+            sleep(1)
+            status.update(label="💰 Grabbing your wallet...")
+
+            sleep(2)
+            status.update(label="💸 Paying...")
+
+            sleep(1)
+            status.update(label="✅ Payment complete!", state="complete")
+            sleep(1)
+
+
+def create_items_db():
+    with open("items.json", "r") as f:
+        all_items = json.load(f)
+        for item in all_items:
+            Item(**item)
